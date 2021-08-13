ef65a02dac52c7c4aea0117a96cc8fb3b9d39607
Wyllie Morth <rodrigosantosmorgado@gmail.com>
2021-08-13 14:51:28 +0100

    Restructured code to accomodate workflow; added flags that can be passed;

--- nvdcu.py
+++ nvdcu.py
@@ -1,16 +1,14 @@
-from moviepy.editor import *
-from helpers import input_selection, print_error, input_range, current_time
-from config import DEFAULT_CLIP_MODE, DEFAULT_NUM_THREADS, SAVE_CLIPS_TO
+import argparse
+from moviepy.editor import VideoFileClip
+from helpers import input_selection, input_range, input_file, YoutubeClip, current_time, print_error, print_info, read_watchlist_file, write_watchlist_file, Watchlist, WatchlistFile, get_videos_in_directory, print_warning, delete_video
+from config import DEFAULT_CLIP_MODE, DEFAULT_NUM_THREADS, SAVE_CLIPS_TO, COMPRESS_FPS, COMPRESS_RES_HEIGHT, ARCHIVE_FOLDER
 from upload import get_authenticated_service, initialize_upload
 
-
-MAX_THREADS = 4  # FIXME: yeet bad code
+MAX_THREADS = 8  # FIXME: yeet bad code
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
 
-def get_clip_preferences(clip_name: str, clip_duration: float):
-    curtime = current_time()
-    clip_name.format(curtime)
+def get_clip_preferences(vdf_clip: VideoFileClip, clip_name: str, clip_duration: float):
 
     title = input("Insert the title for the clip: ")
     if not title:
@@ -34,33 +32,169 @@ def get_clip_preferences(clip_name: str, clip_duration: float):
     options = {op[0:2]: op for op in VALID_PRIVACY_STATUSES}
     message = "Select privacy status: "
     privacy_status = options[input_selection(
-        options=options, message=message, default='un'
-    )]
+        options=options, message=message, default='un')]
 
-    pref = {
-        'clip_name': f"{SAVE_CLIPS_TO}{clip_name}",
-        'title': title,
-        'description': description,
-        'time_end': time_end,
-        'thread_number': thread_number,
-        'privacy_status': privacy_status
-    }
-    print("\nPreferences: ", pref)
+    clip = YoutubeClip(vdf_clip, title=title, description=description, time_from_end=time_end,
+                       number_of_threads=thread_number, privacy_status=privacy_status, clip_file_name=clip_name.format(current_time()))
+    print(clip)
 
-    return pref
+    return clip
 
 
-clip = VideoFileClip("sample.mp4")
-new_clip = clip.subclip(t_end=-5)
-clip_name = "clip_{}.mp4"  # TODO: change this name
+def archive_uploaded(force=False):
+    print_info("Archiving uploaded files...")
+    watchlist = read_watchlist_file()
 
-clip_pref = get_clip_preferences(
-    clip_name=clip_name, clip_duration=clip.duration)
-new_clip.write_videofile(
-    clip_name, fps=60, threads=clip_pref['thread_number'])
+    for f in watchlist.files:
+        if not f.missing:
+            if force or f.uploaded and not f.archived:
+                archive_video(f)
 
-youtube = get_authenticated_service()
-try:
-    initialize_upload(youtube=youtube, clip_preferences=clip_pref)
-except HttpError as e:
-    print(f"An HTTP error {e.resp.status} occurred:\n{e.content}")
+    # Update watchlist file
+    write_watchlist_file(watchlist)
+    print_info("Done.")
+
+
+def archive_video(f: WatchlistFile):
+    print_info(f"Archiving video: {f.filepath}")
+
+    vdf = VideoFileClip(f.filepath)
+    print("Before:", vdf.filename, vdf.fps, vdf.size)
+
+    if (vdf.size[1] > COMPRESS_RES_HEIGHT):
+        vdf = vdf.resize(height=COMPRESS_RES_HEIGHT)
+
+    if (vdf.fps > COMPRESS_FPS):
+        vdf = vdf.set_fps(COMPRESS_FPS)
+
+    print("After:", vdf.filename, vdf.fps, vdf.size, "\n")
+    archived_vdf = vdf.write_videofile(ARCHIVE_FOLDER + f.filename)
+    vdf.close()
+
+    f.archived = True
+
+
+def checkup(f: WatchlistFile, watchlist: Watchlist, ignore_uploaded=False):
+
+    print_info(f"Running checkup for: {f.filename}")
+
+    if f.uploaded and not ignore_uploaded:
+        if f.archived:
+            message = "This video has been uploaded and archived, do you wish to delete the original file? "
+            options = {"y": "yes", "n": "no"}
+            confirm = input_selection(
+                options, message, default="n")
+
+            if confirm == "n":
+                return
+            # Delete the video
+            delete_video(f)
+            return
+        else:
+            description = "- Archiving will compress the video and save it to ARCHIVE_FOLDER in your config.\n- Deleting will permanently delete the original video."
+            message = "Do you wish to archive or delete this video? "
+            options = {"a": "archive", "d": "delete", "n": "none"}
+            confirm = input_selection(
+                options, message, default="n", description=description)
+
+            if confirm == "a":
+                archive_video(f, watchlist)
+            elif confirm == "d":
+                delete_video(f, watchlist)
+
+            return
+
+    if not f.uploaded:
+        description = "You can upload it later."
+        message = "Do you wish to upload this video? "
+        options = {"y": "yes", "n": "no"}
+        confirm = input_selection(
+            options, message, default="n", description=description)
+
+        if confirm == 'n':
+            return
+
+        # Clip preferences
+        # Upload video
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Manage clips in certain directory.')
+
+    parser.add_argument(
+        '-i', '--ignore', '--ignore-uploaded', action="store_true")
+    parser.add_argument('-a', '--archive',
+                        '--archive-uploaded', action="store_true", help="archive uploaded videos")
+    parser.add_argument('-c', '--config', action="store_true")
+    parser.add_argument('--archive-all', action="store_true",
+                        help="archive every video")
+    parser.add_argument('--no-info', action="store_true")
+
+    args = parser.parse_args()
+
+    if args.archive:
+        description = "This will only archive the ones that haven't been archived.\nIf you wish to force the archival of every uploaded video use --force-archive."
+        message = "Are you sure you want to archive already uploaded files? "
+        options = {"y": "yes", "n": "no"}
+        confirm = input_selection(
+            options, message, default="y", description=description)
+
+        if confirm == "n":
+            print_info("Cancelling...")
+            exit()
+
+        archive_uploaded()
+        exit()
+
+    if args.archive_all:
+        description = "This will archive EVERY video even if has been archived.\nIf you wish to archive only unarchived videos use --archive -a."
+        message = "Are you sure you want to archive all already uploaded files? "
+        options = {"y": "yes", "n": "no"}
+        confirm = input_selection(
+            options, message, default="y", description=description)
+
+        if confirm == "n":
+            print_info("Cancelling...")
+            exit()
+
+        archive_uploaded(force=True)
+        exit()
+
+    if args.config:
+        print_info("Configuration mode...")
+        exit()
+
+    if args.ignore:
+        print_info("Ignoring uploaded files...")
+        # NO BREAK
+
+    # Check files in directory VIDEOS_FOLDER:
+    videos = get_videos_in_directory()
+
+    # Read files already in watchlist
+    watchlist = read_watchlist_file()
+
+    for video in videos:
+        video_to_check = None
+
+        for wl_file in watchlist.nonmissing_files():
+            if wl_file.filename == video[0]:
+                video_to_check = wl_file
+                break
+
+        # Not in watchlist
+        if not video_to_check:
+            # Add to watchlist
+            video_to_check = WatchlistFile(video[1])
+            watchlist.add_file(video_to_check)
+
+        # Run checkup
+        checkup(video_to_check, watchlist, args.ignore)
+
+    write_watchlist_file(watchlist)
+    # yt_clip = get_clip_preferences(new_clip,clip_name = clip_name, clip_duration = clip.duration)
+    # yt_clip.write_clip_file(fps=60)
+
+    # auth_service = get_authenticated_service()
+    # yt_clip.upload(auth_service=auth_service)
